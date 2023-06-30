b3b52eb65c2d6ef21d68272d7b910723a16c463f
LeonBundi <lmemebundi@gmail.com>
2023-06-30 14:23:23 -0700

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ char *leet(char *);
 char *rot13(char *);
 char *_strstr(char *haystack, char *needle);
 void print_number(int n);
+int main(void);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
