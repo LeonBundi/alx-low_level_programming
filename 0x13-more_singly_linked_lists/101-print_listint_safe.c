bdcbd95316a5131277eb08be4dedd5cf8eb4414a
LeonBundi <lmemebundi@gmail.com>
2023-08-02 07:04:03 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,26 +9,22 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
-	const listint_t *visited_nodes = NULL;
+	int diff;
 
 	while (head)
 	{
-		const listint_t *temp = visited_nodes;
-
-		while (temp)
-		{
-			if (head == temp)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				return (num);
-			}
-			temp = temp->next;
-		}
-
+		diff = head - head->next;
 		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		visited_nodes = head;
-		head = head->next;
+		if (diff > 0)
+		{
+			head = head->next;
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
 	}
 
 	return (num);
